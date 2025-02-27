e7fb21310e5f50dc35f7d117a7f70fd78a966d1a
Denis <666trall666@gmail.com>
2025-02-27 14:33:03 +0300

    feat наичнаю заливать на тест

--- src/js/mainPage.js
+++ src/js/mainPage.js
@@ -1,12 +1,11 @@
-﻿﻿document.addEventListener("DOMContentLoaded", () => {
-  let block = document.querySelector(".content-bar__container_hidden");
-  let btnOpenBlock = document.querySelector(".content-bar__button");
+﻿﻿let block = document.querySelector(".content-bar__container_hidden");
+let btnOpenBlock = document.querySelector(".content-bar__button");
 
-  btnOpenBlock.addEventListener("click", () => {
-    btnOpenBlock.style.display = "none";
-    block.classList.add("active");
-  });
+btnOpenBlock.addEventListener("click", () => {
+  btnOpenBlock.style.display = "none";
+  block.classList.add("active");
 });
+document.addEventListener("DOMContentLoaded", () => {});
 function bildSliders() {
   //BildSlider
   let sliders = document.querySelectorAll(
@@ -43,14 +42,70 @@ function initSliders() {
       },
       loop: true,
       pagination: {
-        el: ".slide-banner__pagination",
+        el: ".slider-banner__pagination",
         clickable: true,
+      },
+      navigation: {
+        nextEl: ".slider-banner__back",
+        prevEl: ".slider-banner__next",
       },
       breakpoints: {
         319.98: {},
         429.98: {},
 
         767.98: {},
+        1023.98: {},
+      },
+      on: {},
+    });
+  }
+  if (document.querySelector(".partners__slider")) {
+    new Swiper(".partners__slider", {
+      observer: true,
+      observeParents: true,
+      slidesPerView: 4,
+      spaceBetween: 24,
+      autoHeight: false,
+      centeredSlides: false,
+      loop: false,
+      speed: 300,
+      grid: {
+        rows: 2, // Количество строк
+        fill: "row", // Заполнение по строкам (или 'column')
+      },
+      pagination: {
+        el: ".slide-banner__pagination",
+        clickable: true,
+      },
+      breakpoints: {
+        319.98: {
+          slidesPerView: "1.3",
+          grid: {
+            rows: 1, // Количество строк
+            fill: "row", // Заполнение по строкам (или 'column')
+          },
+          loop: true,
+          centeredSlides: true,
+        },
+        429.98: {
+          grid: {
+            rows: 1, // Количество строк
+            fill: "row", // Заполнение по строкам (или 'column')
+          },
+          loop: true,
+          centeredSlides: true,
+          slidesPerView: "1.5",
+        },
+
+        767.98: {
+          slidesPerView: "3.5",
+          loop: true,
+          centeredSlides: true,
+          grid: {
+            rows: 1, // Количество строк
+            fill: "row", // Заполнение по строкам (или 'column')
+          },
+        },
         1023.98: {},
       },
       on: {},
